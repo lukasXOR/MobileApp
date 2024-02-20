a921655a37e8e99e3298f9d8fff31d43f4f0a33a
lukas <lukas7865@yahoo.co.uk>
2024-02-20 12:47:32 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.view.View;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView Location = findViewById(R.id.Location);
+        Location.setText("Northampton");
+    }
+}
+
+class Weather {
+    public static void get() {
+        try {
+
+        } catch (Exeception e) {
+        }
+        }
+        URL WeatherAPIURL = new URL("http://example.com/api/data");
+
+        HttpURLConnection connection = (HttpURLConnection) WeatherAPIURL.openConnection();
+
+        connection.setRequestMethod("GET");
+
+
+
+    }
+}
+
+class Colors {
+    public static int Sunny = 0xFFcfd13d;
+    public static int Cloudy = 0xFFC2C3C1;
+    public static int Rain = 0xFF00A5B1;
+}
